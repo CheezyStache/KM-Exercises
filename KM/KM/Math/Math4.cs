a98321d89698b0b6abcbc3c047aadfe7b2a5f9ef
nlioxa <iluhakorzun@gmail.com>
2019-05-20 22:24:34 +0300

    added Cochran function that computes Cochran statistics

--- KM/KM/Math/Math4.cs
+++ KM/KM/Math/Math4.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using KM.Model;
 using KM.Services;
+using Accord.Statistics.Distributions.Univariate;
 
 namespace KM
 {
@@ -35,7 +36,7 @@ namespace KM
             tableAll = (TableObject[])manageService.GetResultFromStep(0);
             m = Input.YCount;
 
-            for(int i = 0; i < tableAll.Length; ++i)
+            for (int i = 0; i < tableAll.Length; ++i)
             {
                 //????????????????????????????????????????????????????????????????????
                 double[] yTemp = tableAll[i].Y.Take(tableAll[i].Y.Length - 1).Cast<double>().ToArray();
@@ -45,11 +46,11 @@ namespace KM
             s = new double[yCollection.Count];
 
             //count s^2
-            for(int i = 0; i < yCollection.Count; ++i)
+            for (int i = 0; i < yCollection.Count; ++i)
             {
                 double[] currentOne = yCollection[i];
                 double ySumAVG = 0;
-                for(int j = 0; j < currentOne.Length; ++j)
+                for (int j = 0; j < currentOne.Length; ++j)
                 {
                     ySumAVG += Math.Pow(currentOne[j] - currentOne.Average(), 2);
                 }
@@ -65,7 +66,7 @@ namespace KM
 
             //place to KOXPEH
 
-
+            var g_tabular = Cochran(0.05, Input.YCount, Input.ResearchCount);
 
             //if yes caclulate s0^2
             s0 = sSum / s.Length;
@@ -92,6 +93,16 @@ namespace KM
             status.isSuccsess = isSuccess;
             status.messages = new string[] { "Element 4", "ok" };
             return status;
+        }
+
+        private double Cochran(double significance_level, int groups_number, int observations_number)
+        {
+            var fst_degree = (observations_number - 1);
+            var snd_degree = (groups_number - 1) * (observations_number - 1);
+            var fd = new FDistribution(fst_degree, snd_degree);
+            var fisher_value = fd.QuantileDensityFunction(significance_level);
+            var result = 1 / (1 + (groups_number - 1) / fisher_value);
+            return result;
         }
     }
 }
