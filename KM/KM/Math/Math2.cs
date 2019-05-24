a93debc4e23adb2afe0698409e6891eead257ccb
Лёша Сягровский <cutpaks@ukr.net>
2019-05-25 01:55:38 +0300

    All windows connected (may be some problems)

--- KM/KM/Math/Math2.cs
+++ KM/KM/Math/Math2.cs
@@ -12,6 +12,12 @@ namespace KM
     {
         private ManageService manageService;
 
+        private int[][] planValues; // матрица планирования(+1, -1)
+
+        private bool[][] GenerationRel; // послднее значение в каждом массиве отвечает за минус (минус это true)
+
+        private TableObject[] tableObjects;
+
         public Math2(ManageService manageService)
         {
             this.manageService = manageService;
@@ -19,12 +25,96 @@ namespace KM
 
         public object[] GetResult()
         {
-            //TODO planValues should be returned
-            return Input.GenerationX;
+            return tableObjects;
+        }
+
+        private void CalculatePlanValues()
+        {
+            tableObjects = manageService.GetResultFromStep(0) as TableObject[];
+
+            int changeRange = 0;
+            int count = 0;
+            int value = 1;
+            planValues = new int[Input.XCount + Input.GenerationRatio + 1][];
+
+            GenerationRel = Input.GenerationX;
+
+            for (int i = 0; i < Input.XCount + 1; ++i)
+            {
+                planValues[i] = new int[Input.ResearchCount];
+                value = 1;
+
+                if (i != 0)
+                {
+                    changeRange = (int)Math.Pow(2, i - 1);
+                    count = changeRange;
+                }
+
+                for (int j = 0; j < planValues[i].Length; ++j)
+                {
+                    if (i == 0)
+                    {
+                        planValues[i][j] = value;
+                    }
+                    else
+                    {
+                        if (count == 0)
+                        {
+                            count = changeRange;
+                            value *= -1;
+                        }
+
+                        count = AssignValueAndDecrement(i, j, count, value);
+
+                    }
+                }
+            }
+
+            for (int i = Input.XCount + 1, l = 0; i < planValues.Length; ++i, ++l) //ТОDO протестировать
+            {
+                planValues[i] = new int[Input.ResearchCount];
+
+                for (int j = 0; j < Input.ResearchCount; ++j)
+                {
+                    planValues[i][j] = 1;
+                    for (int k = 0; k < Input.GenerationX.Length - 1; ++k)
+                    {
+                        if (Input.GenerationX[l][k])
+                        {
+                            planValues[i][j] *= planValues[k + 1][j];
+                        }
+                    }
+
+                    if(Input.GenerationX[l][Input.GenerationX.Length - 1])
+                        planValues[i][j] *= -1;
+                }
+            }
+
+            
+            for(int i = 0; i < tableObjects.Length; i++)
+            {
+                tableObjects[i].X = new int[planValues.Length];
+            }
+
+            for (int i = 0; i < tableObjects.Length; i++)
+            {
+                for (int j = 0; j < planValues.Length; j++)
+                {
+                    tableObjects[i].X[j] = planValues[j][i];
+                }
+            }
+        }
+
+        private int AssignValueAndDecrement(int i, int j, int count, int value)
+        {
+            planValues[i][j] = value;
+            return --count;
         }
 
         public Status Process()
         {
+            CalculatePlanValues();
+
             //Port logic for planValues to this class
             Status status = new Status();
             status.isSuccsess = true;
@@ -34,7 +124,7 @@ namespace KM
 
         public string GetName()
         {
-            return "Выбор генерирующих соотношений";
+            return "Таблица исходных данных";
         }
 
         public string[] GetStringResult()
--- KM/KM/Model/Input.cs
+++ KM/KM/Model/Input.cs
@@ -62,7 +62,7 @@ namespace KM
     public class TableObject
     {
         public int Number { get; set; }
-        public double[] X { get; set; }
+        public int[] X { get; set; }
         public double[] Y { get; set; }
     }
 
